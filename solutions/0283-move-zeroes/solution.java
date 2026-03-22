e501e10957229df38952283a1f720276b00b9718
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2026-03-22 12:42:55 +0000

    Sync LeetCode submission Runtime - 2 ms (91.97%), Memory - 47.9 MB (20.66%)

---
+++ solutions/0283-move-zeroes/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int k = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] != 0){
+                nums[k++] = nums[i];
+            }
+        }
+        for(int i = k;i<n;i++){
+            nums[i] = 0;
+        }
+    }
+}
