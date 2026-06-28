b7079e4442ea0151accc32d02be2dad5e461582f
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2026-06-28 15:27:22 +0000

    Sync LeetCode submission Runtime - 1 ms (40.59%), Memory - 63.2 MB (12.42%)

--- solutions/0189-rotate-array/solution.java
+++ solutions/0189-rotate-array/solution.java
@@ -1,18 +1,9 @@
 class Solution {
-    public static void reverseFn(int[] arr, int start, int end) {
-        while(start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
     public void rotate(int[] nums, int k) {
-    int n = nums.length;
-        k = k % n;
-       reverseFn(nums,0,n-k-1);
-       reverseFn(nums,n-k,n-1);
-       reverseFn(nums,0,n-1);
+        int[] copy = nums.clone();
+        int n = nums.length;
+        for(int i = 0;i<n;i++){
+            nums[(i+k) % n] = copy[i];
+        }
     }
 }
