dd98b3609c1e749c5cd4077f8e8e2ad6a12d74d6
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2026-07-21 16:08:22 +0000

    Sync LeetCode submission Runtime - 5 ms (82.92%), Memory - 77.3 MB (49.46%)

---
+++ solutions/0011-container-with-most-water/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int water = 0;
+        while(left<right){
+             int capacity = Math.min(height[left],height[right])*(right-left);
+             water = Math.max(water,capacity);
+             if(height[left]<height[right]){
+                left++;
+             }else{
+                right--;
+             }
+        }
+        return water;
+    }
+}
