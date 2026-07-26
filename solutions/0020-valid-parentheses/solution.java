c100d38c78f118a78d45e6d4dbb222fe671c724e
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2026-07-26 06:13:08 +0000

    Sync LeetCode submission Runtime - 2 ms (98.25%), Memory - 43.3 MB (41.37%)

---
+++ solutions/0020-valid-parentheses/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (x == '(' || x == '{' || x == '[') {
+                st.push(x);
+            } else if (st.isEmpty()) {
+                return false;
+            }else if (x == ')' && st.peek() == '(') {
+                st.pop();
+            }else if (x == '}' && st.peek() == '{') {
+                st.pop();
+            }else if (x == ']' && st.peek() == '[') {
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
