2a53124d16ea90bda299b7dea6f6b3d7be35f7e2
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2026-08-03 18:57:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (11.29%)

---
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0,r=n-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m]==target){
+              return m;
+            }else if(nums[l]<=nums[m]){
+                if(nums[l]<=target && nums[m]>target){
+                    r=m-1;
+                }else{
+                    l=m+1;
+                }
+            }else{
+                if(nums[m]<target && nums[r]>=target){
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
